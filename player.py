981dbbb3ce0bcf1863d458b6929163f09fe68d73
EthanHung0 <aaronoriginal2009@gmail.com>
2025-07-23 17:21:13 +0700

    e

--- player.py
+++ player.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+# import re
 
 
 class Player:
-    def __init__(self, player_id, name, number, position, dob, performance_score=0.0):
+    def __init__(self, player_id, name, number, position, dob, performance_score):
         self.player_id = player_id
         self.name = name
         self.number = number
@@ -10,10 +11,26 @@ class Player:
         self.dob = dob
         self.performance_score = performance_score
 
+    @property
+    def performance_score(self):
+        return self._performance_score if hasattr(self,"_performance_score") else None
+    @performance_score.setter
+    def performance_score(self,value):
+        try:
+            value = float(value)
+        except ValueError:
+            raise ValueError("Điểm phong độ không hợp lệ.")
+        self._performance_score = value
+
     def update_performance(self, score):
-        self.performance_score = score
+        self._performance_score = score
 
     def get_age(self):
+        # pattern = r"^\d{4}-\d{2}-\d{2}$"
+        # if not re.match(pattern,self.dob):
+        #     raise ValueError("Ngày tháng không hợp lệ.")
         birth_date = datetime.strptime(self.dob, "%Y-%m-%d")
         today = datetime.today()
         age = today.year - birth_date.year
+
+
